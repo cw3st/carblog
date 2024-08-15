743dfa9240c9ae3130c8e5733c6182efb9a3ba04
Emmanuel  Chukwunenye <156005055+cw3st@users.noreply.github.com>
2024-08-15 12:20:06 +0100

    Add files via upload

---
+++ big.js
@@ -0,0 +1,247 @@
+// //state (= / * > < >= =>)
+
+// const bal = 58
+// const wid = 12
+
+// if(bal===wid) {
+//     alert("can withdraw")
+// } else {
+//     alert("cannot withdraw")
+// }
+
+// function car() {
+//     alert('Please visit our Cars page')
+// }
+// function truck() {
+//     alert('Please visit our Trucks page')
+// }
+
+// function sub1() {
+//     const price = 30000
+//     const cheap = 29000
+
+//     if(cheap<<price) {
+//     ('Dear Esteemed Customer Proceed To Make Payment')
+//     } else{
+//         ('Dear Customer Make Money')
+//     }
+// }
+
+// function alist1() {
+//     let xy  = document.getElementById("dbf3i")
+//     xy.style.backgroundColor = "silver"
+//     xy.style.color = "black"
+// }
+function sub1() {
+    let abc = document.getElementById("subd")
+    abc.style.display = "inline-block"
+    return
+}
+
+function navl2 () {
+    let xy = document.getElementById("l2")
+    xy.style.display ="flex"
+    let xx = document.getElementById("jh1")
+    xx.style.display ="block"
+    let w = document.getElementById("jh2")
+    w.style.display = "none"
+    let gg = document.getElementById("jh3")
+    gg.style.display = "none"
+    let mm = document.getElementById("l1")
+    mm.style.display = "none" 
+    let mb = document.getElementById("l3")
+    mb.style.display = "none" 
+    let mn = document.getElementById("l4")
+    mn.style.display = "none"
+}
+
+function navl3 () {
+    let xy = document.getElementById("l3")
+    xy.style.display ="flex"
+    let mm = document.getElementById("l1")
+    mm.style.display = "none" 
+    let mb = document.getElementById("l2")
+    mb.style.display = "none" 
+    let mn = document.getElementById("l4")
+    mn.style.display = "none"
+    let xx = document.getElementById("jh1")
+    xx.style.display ="none"
+    let w = document.getElementById("jh2")
+    w.style.display = "block"
+    let gg = document.getElementById("jh3")
+    gg.style.display = "none"
+}
+
+function navl4 () {
+    let xy = document.getElementById("l4")
+    xy.style.display ="flex"
+    let mm = document.getElementById("l1")
+    mm.style.display = "none" 
+    let mb = document.getElementById("l3")
+    mb.style.display = "none" 
+    let mn = document.getElementById("l2")
+    mn.style.display = "none"
+    let xx = document.getElementById("jh1")
+    xx.style.display ="none"
+    let w = document.getElementById("jh2")
+    w.style.display = "none"
+    let gg = document.getElementById("jh3")
+    gg.style.display = "block"
+}
+
+function navl1 () {
+    let xy = document.getElementById("l1")
+    xy.style.display ="flex"
+    let mm = document.getElementById("l2")
+    mm.style.display = "none" 
+    let mb = document.getElementById("l3")
+    mm.style.display = "none"
+    let mn = document.getElementById("l4")
+    mm.style.display = "none" 
+    let xx = document.getElementById("jh1")
+    xx.style.display ="none"
+    let w = document.getElementById("jh2")
+    w.style.display = "none"
+    let gg = document.getElementById("jh3")
+    gg.style.display = "none"
+}
+
+function navlb () {
+    let xy = document.getElementById("l1")
+    xy.style.display ="none"
+    let mm = document.getElementById("l2")
+    mm.style.display = "none" 
+    let mb = document.getElementById("l3")
+    mb.style.display = "none"
+    let mn = document.getElementById("l4")
+    mn.style.display = "none"  
+}
+
+
+function hh1() {
+    let xy = document.getElementById("jh1")
+    xy.style.display = "none"
+}
+
+function hh2() {
+    let xy = document.getElementById("jh2")
+    xy.style.display = "none"
+}
+
+function hh3() {
+    let xy = document.getElementById("jh3")
+    xy.style.display = "none"
+}
+
+function pay() {
+    let xy = document.getElementById("rlist");
+    xy.style.display = "block";
+    let cc = document.getElementById("srnav");
+    cc.style.display = "block";
+    let br = document.getElementById("rnav");
+    br.style.display = "none";
+  }
+  
+  function cod() {
+    let xy = document.getElementById("rlist");
+    xy.style.display = "none";
+    let br = document.getElementById("srnav");
+    br.style.display = "none";
+    let hh = document.getElementById("rnav");
+    hh.style.display = "block";
+  }
+
+
+
+function alist(){
+    let xy = document.getElementById("dbf3ii")
+    xy.style.backgroundColor = "rgb(76, 72, 72)"
+    xy.style.color = "silver"
+        let ab = document.getElementById("dbf3i")
+        ab.style.backgroundColor = "silver"
+        ab.style.color = "black" 
+        let ad = document.getElementById("dbf3iii")
+        ad.style.backgroundColor = "silver"
+        ad.style.color = "black"    
+        let sa = document.getElementById("dbf3iv")
+        sa.style.backgroundColor = "silver"
+        sa.style.color = "black" 
+        let zx = document.getElementById("dbf3v")
+        zx.style.backgroundColor = "silver"
+        zx.style.color = "black" 
+    }
+
+function alist1(){
+    let mm = document.getElementById("dbf3i")
+    mm.style.backgroundColor = "rgb(76, 72, 72)"
+    mm.style.color = "silver"
+        let ab = document.getElementById("dbf3ii")
+        ab.style.backgroundColor = "silver"
+        ab.style.color = "black" 
+        let ad = document.getElementById("dbf3iii")
+        ad.style.backgroundColor = "silver"
+        ad.style.color = "black"    
+        let sa = document.getElementById("dbf3iv")
+        sa.style.backgroundColor = "silver"
+        sa.style.color = "black" 
+        let zx = document.getElementById("dbf3v")
+        zx.style.backgroundColor = "silver"
+        zx.style.color = "black" 
+}
+
+function alist2(){
+    let mm = document.getElementById("dbf3iii")
+    mm.style.backgroundColor = "rgb(76, 72, 72)"
+    mm.style.color = "silver"
+        let ab = document.getElementById("dbf3ii")
+        ab.style.backgroundColor = "silver"
+        ab.style.color = "black" 
+        let ad = document.getElementById("dbf3i")
+        ad.style.backgroundColor = "silver"
+        ad.style.color = "black"    
+        let sa = document.getElementById("dbf3iv")
+        sa.style.backgroundColor = "silver"
+        sa.style.color = "black" 
+        let zx = document.getElementById("dbf3v")
+        zx.style.backgroundColor = "silver"
+        zx.style.color = "black" 
+}
+
+function alist3() {
+    let mm = document.getElementById("dbf3iv")
+    mm.style.backgroundColor = "rgb(76, 72, 72)"
+    mm.style.color = "silver"
+        let ab = document.getElementById("dbf3ii")
+        ab.style.backgroundColor = "silver"
+        ab.style.color = "black" 
+        let ad = document.getElementById("dbf3i")
+        ad.style.backgroundColor = "silver"
+        ad.style.color = "black"    
+        let sa = document.getElementById("dbf3iii")
+        sa.style.backgroundColor = "silver"
+        sa.style.color = "black" 
+        let zx = document.getElementById("dbf3v")
+        zx.style.backgroundColor = "silver"
+        zx.style.color = "black" 
+}
+
+function alist4(){
+    let mm = document.getElementById("dbf3v")
+    mm.style.backgroundColor = "rgb(76, 72, 72)"
+    mm.style.color = "silver"
+        let ab = document.getElementById("dbf3ii")
+        ab.style.backgroundColor = "silver"
+        ab.style.color = "black" 
+        let ad = document.getElementById("dbf3i")
+        ad.style.backgroundColor = "silver"
+        ad.style.color = "black"    
+        let sa = document.getElementById("dbf3iv")
+        sa.style.backgroundColor = "silver"
+        sa.style.color = "black" 
+        let zx = document.getElementById("dbf3iii")
+        zx.style.backgroundColor = "silver"
+        zx.style.color = "black" 
+}
+
+document.querySelector
+("body > div:nth-child(3) > div.sc-157agsr-0.jvChWP > div > div > main > div > div > section:nth-child(13) > div.sc-1kfqkp9-0.ftfBRR.sc-1s9hjy6-0.dgYMEC > div.sc-1kfqkp9-1.ivnCEC > div > article:nth-child(1)")
